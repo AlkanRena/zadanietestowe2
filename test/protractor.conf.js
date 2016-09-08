97148abd57c8cacc16882bf4a95a6c78c0dbe2b1
Dawid Kubic <dawid.kubic@gmail.com>
2016-09-08 18:40:09 +0200

    I can check situation on shown elements - using Jasmine and Protractor

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,8 +1,10 @@
+
 exports.config = {
     framework:'jasmine',
 
     specs: ['./spec/index/*.spec.js'],
     // seleniumAddress: 'http://localhost:4444/wd/hub'
     baseUrl: 'http://localhost:3333'
+
 
 };
--- test/spec/index/adding.spec.js
+++ test/spec/index/adding.spec.js
@@ -1,45 +1,101 @@
 var inputName = element(by.model('userDataNew.name'));
 var inputEmail = element(by.model('userDataNew.email'));
 
+describe("check if visible", function () {
+    browser.get('http://localhost:3333');
 
 
-describe("addingTool-protractor", function() {
+    var elementNew = browser.findElement(by.css('#AddForm'));
+    var elementEdit = browser.findElement(by.css('#EditForm'));
+    var createNewButton = element(by.buttonText("Create new"));
 
-    beforeEach(function(){
-        browser.get('http://localhost:3333');
+    it('should be hidden 1', function () {
+
+        elementNew.isDisplayed().then(function(result){
+            if(result) {
+                console.log('Wait I am naked')
+            } else {
+                console.log('I am invisible')
+            }
+        });
     });
-    
-    it('should click on it', function () {
-        Terminator();
-        Terminator();
-        browser.pause();
+
+    it('should be hidden 2', function () {
+
+        elementEdit.isDisplayed().then(function(result) {
+            if (result) {
+                console.log("What i am true")
+            } else{
+                console.log("What i am false")
+            }
+        })
     });
 
+    // it('should be visible', function () {
+    //     createNewButton.click().then(function (result) {
+    //         if (result) {
+    //             console.log("Open window")
+    //         } else {
+    //             console.log("What window")
+    //         }
+    //     })
+    //
+    // })
 
 
 });
-var nameForBot = "I am back";
-var emailForBot = "what";
 
-
-function Terminator() {
-    setTimeout(BotFunction, 2000);
-}
-
-function BotFunction(nameForBot, emailForBot) {
-    element(by.buttonText("Create new")).click();
-
-    inputName.sendKeys('nameForBot');
-    expect(input.getAttribute('value').toBe('name'));
-
-    inputEmail.sendKeys('emailForBot');
-    expect(input.getAttribute('value').toBe('email'));
-
-    browser.pause();
-
-    setTimeout(function () {
-        element(by.buttonText("Save")).click();
-    }, 3000);
-
-
-}
+// describe("addingTool-protractor", function() {
+//
+//     beforeAll("at start say this", function () {
+//         browser.get('http://localhost:3333');
+//
+//     }
+//     );
+//
+//     beforeEach(function(){
+//
+//
+//         createNewButton.click();
+//     });
+//
+//     browser.pause();
+//
+//     it('should click on it', function () {
+//         Terminator();
+//
+//     });
+//
+//     it('should click on it', function () {
+//         Terminator();
+//
+//     });
+//
+//     afterEach('should ad new', function () {
+//         element(by.buttonText("Save")).click();
+//     });
+//
+//
+// });
+//
+// var nameForBot;
+// var emailForBot;
+//
+// var createNewButton = element(by.buttonText("Create new"));
+//
+// function Terminator() {
+//     nameForBot = "I am backnot";
+//     emailForBot = "what";
+//
+//     setTimeout(BotFunction(), 2000);
+// }
+//
+// function BotFunction(nameForBot, emailForBot) {
+//
+//     inputName.setValue(nameForBot);
+//     expect(input.getAttribute('value').toBe('I am back'));
+//
+//     inputEmail.setValue(emailForBot);
+//     expect(input.getAttribute('value').toBe('what'));
+//
+// }
