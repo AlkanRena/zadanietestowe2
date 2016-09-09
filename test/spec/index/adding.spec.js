1d37c7332c336affcb77271d559045141e72c3f4
Dawid Kubic <dawid.kubic@gmail.com>
2016-09-09 08:03:45 +0200

    few steps forward test are working - now you can test if windows are present and if one of click events is working

--- test/spec/index/adding.spec.js
+++ test/spec/index/adding.spec.js
@@ -1,50 +1,6 @@
 var inputName = element(by.model('userDataNew.name'));
 var inputEmail = element(by.model('userDataNew.email'));
 
-describe("check if visible", function () {
-    browser.get('http://localhost:3333');
-
-
-    var elementNew = browser.findElement(by.css('#AddForm'));
-    var elementEdit = browser.findElement(by.css('#EditForm'));
-    var createNewButton = element(by.buttonText("Create new"));
-
-    it('should be hidden 1', function () {
-
-        elementNew.isDisplayed().then(function(result){
-            if(result) {
-                console.log('Wait I am naked')
-            } else {
-                console.log('I am invisible')
-            }
-        });
-    });
-
-    it('should be hidden 2', function () {
-
-        elementEdit.isDisplayed().then(function(result) {
-            if (result) {
-                console.log("What i am true")
-            } else{
-                console.log("What i am false")
-            }
-        })
-    });
-
-    // it('should be visible', function () {
-    //     createNewButton.click().then(function (result) {
-    //         if (result) {
-    //             console.log("Open window")
-    //         } else {
-    //             console.log("What window")
-    //         }
-    //     })
-    //
-    // })
-
-
-});
-
 // describe("addingTool-protractor", function() {
 //
 //     beforeAll("at start say this", function () {
--- test/spec/index/index.spec.js
+++ test/spec/index/index.spec.js
@@ -1,1 +1,55 @@
+describe("check if visible", function () {
+    browser.get('http://localhost:3333');
 
+
+    var elementNew = browser.findElement(by.css('#AddForm'));
+    var elementEdit = browser.findElement(by.css('#EditForm'));
+    var createNewButton = browser.findElement(by.buttonText("Create new"));
+    var cancelNewButton = browser.findElement(by.buttonText("Cancel Adding"));
+
+    it('should be hidden 1', function () {
+
+        elementNew.isDisplayed().then(function(result1){
+            if(result1) {
+                console.log('Wait I am naked')
+            } else {
+                console.log('I am invisible')
+            }
+        });
+    });
+
+    it('should be hidden 2', function () {
+
+        elementEdit.isDisplayed().then(function(result2) {
+            if (result2) {
+                console.log("What i am true")
+            } else{
+                console.log("What i am false")
+            }
+        })
+    });
+
+    it('should be visible Adding Window', function () {
+        createNewButton.click();
+        console.log('I clicked');
+            elementNew.isDisplayed().then(function (result3) {
+            if (result3 == true) {
+                console.log('You can see me');
+            } else {
+                console.log("What window Add")
+            }
+        });
+    });
+    it('should be hidden Adding Window', function () {
+        cancelNewButton.click();
+        console.log('I clicked second time');
+        elementNew.isDisplayed().then(function (result3) {
+            if (result3 == true) {
+                console.log('You can see me')
+            } else {
+                console.log("What window Add")
+            }
+        });
+    })
+
+});
