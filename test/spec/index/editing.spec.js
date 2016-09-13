9dc2175aa3bdb875d6225ad434fee500d24d82fd
Dawid Kubic <dawid.kubic@gmail.com>
2016-09-13 08:56:28 +0200

    test code for editing completed + two more test subjects for Protractor Test

--- test/spec/index/editing.spec.js
+++ test/spec/index/editing.spec.js
@@ -54,6 +54,7 @@ describe("Editing-Tool-protractor", function(){
         saveUserTerminator.click()
     });
 
+
     //Ant Z edit files test
 
     var antzEdit = browser.element(by.css('#edit_100006'));
@@ -73,6 +74,8 @@ describe("Editing-Tool-protractor", function(){
             if(resultEditAntz) {
                 console.log('test passed - Edit Open');
                 antzEdit.click();
+            } else {
+                console.log('test failed - wont show')
             }
         })
     });
@@ -101,13 +104,95 @@ describe("Editing-Tool-protractor", function(){
         editUserEmailAntZ.sendKeys('sandor@clagne.got');
     });
 
+    it('Save The Hound',function () {
+       saveUserAntz.click();
+    });
+
+
     //John Snow knows nothing
-    var snowEdit = browser.element(by.css('#edit_100006'));
-    var snowbuttonMenu = browser.element(by.css('#singleRowButton_100006'));
 
-    var editUserNameSnow = browser.element(by.css('#userName_100006'));
-    var editUserEmailSnow = browser.element(by.css('#userEmail_100006'));
+    var snowEdit = browser.element(by.css('#edit_100005'));
+    var snowbuttonMenu = browser.element(by.css('#singleRowButton_100005'));
 
-    var saveUserSnow = browser.element(by.css('#Save_100006'));
+    var editUserNameSnow = browser.element(by.css('#userName_100005'));
+    var editUserEmailSnow = browser.element(by.css('#userEmail_100005'));
+
+    var saveUserSnow = browser.element(by.css('#Save_100005'));
+
+    var editWindowSnow = browser.element(by.css('#EditForm_100005'));
+
+    it('open Snow', function () {
+        browser.actions().mouseMove(snowbuttonMenu).perform();
+    });
+
+    it('Open edit Snow', function () {
+        snowEdit.isDisplayed().then(function (resultEditSnow) {
+            if(resultEditSnow) {
+                console.log('test passed - Edit Open');
+                snowEdit.click();
+            } else {
+             console.log('test failed - wont show ')
+            }
+        })
+    });
+
+    //Caution Spoiler Ahead for Serial and probably for the book
+    //All below test are for testing how angular is working for changing name frequently
+
+    it('check if John Snow is John Snow', function () {
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('Are you John Snow?');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('Yes, I am John Snow');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('Maybe you are John Targaryen ?');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('It is not possible!');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('Its true your mother is Lyan Stark');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('So I am Stark and Targaryen? Is it true tree eyed reven');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('Yes, John you are Stark and Targaryen');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('So from that moment I will be called');
+        editUserNameSnow.clear();
+        browser.sleep(500);
+        editUserNameSnow.sendKeys('John Stark-Targaryen');
+    });
+
+    it('Lets check email and change it', function () {
+        editUserEmailSnow.clear();
+        editUserEmailSnow.sendKeys('John');
+        expect(editUserEmailSnow.getAttribute('value')).toBe('John');
+        saveUserSnow.click();
+        editWindowSnow.isDisplayed().then(function (windowResultSnow){
+            if(windowResultSnow){
+                console.log('test passed - you cant click if there is mistake');
+            } else{
+                console.log('test failed - window closed but it shouldnt')
+            }
+        });
+    });
+
+    it('Lets fix email', function () {
+        expect(editUserEmailSnow.getAttribute('value')).toBe('John');
+        editUserEmailSnow.sendKeys('@stark-targaryen.got');
+        expect(editUserEmailSnow.getAttribute('value')).toBe('John@stark-targaryen.got')
+    });
+
+    it('Lets send him', function(){
+        saveUserSnow.click();
+    });
+
+    //Its working as expected you can edit all data
 
 });
